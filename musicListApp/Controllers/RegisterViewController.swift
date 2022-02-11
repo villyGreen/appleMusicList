c6970578b9fe548b9f05df4c35a882e19da2ebd7
villyGreen <vat.13@list.ru>
2022-02-11 22:32:37 +0700

    validate textFields

--- musicListApp/Controllers/RegisterViewController.swift
+++ musicListApp/Controllers/RegisterViewController.swift
@@ -8,19 +8,25 @@
 
 import UIKit
 
-
 class RegisterViewController: UIViewController {
-    
     var scrollView = UIScrollView()
-    let mainLabel = UILabel(textLabel: "Регистрация", fontLabel: UIFont(name: "Gill Sans", size: 25), textAlpha: 0.7, textColor: .black)
+    let mainLabel = UILabel(textLabel: "Регистрация",
+                            fontLabel: UIFont(name: "Gill Sans", size: 25),
+                            textAlpha: 0.7, textColor: .black)
     let mainLogo = UIImageView(image: UIImage(named: "logo"))
-    let userNameLabel = UILabel(textLabel: "Имя и Фамилия", fontLabel: UIFont.systemFont(ofSize: 17), textAlpha: 0.9, textColor: .black)
+    let userNameLabel = UILabel(textLabel: "Имя пользователя",
+                                fontLabel: UIFont.systemFont(ofSize: 17),
+                                textAlpha: 0.9, textColor: .black)
     let userNameTF = UITextField(fontTf: UIFont(name: "Gill Sans", size: 15))
-    let phoneLabel = UILabel(textLabel: "Номер телефона", fontLabel: UIFont.systemFont(ofSize: 17), textAlpha: 0.9, textColor: .black)
+    let phoneLabel = UILabel(textLabel: "Номер телефона",
+                             fontLabel: UIFont.systemFont(ofSize: 17),
+                             textAlpha: 0.9, textColor: .black)
     let phoneTf = UITextField(fontTf: UIFont(name: "Gill Sans", size: 15))
     let passwordTf = UITextField(fontTf: UIFont(name: "Gill Sans", size: 15))
-    let passwordLabel = UILabel(textLabel: "Пароль", fontLabel: UIFont.systemFont(ofSize: 17), textAlpha: 0.9, textColor: .black)
-    let confirmPasswordLabel = UILabel(textLabel: "Потвердите Пароль", fontLabel: UIFont.systemFont(ofSize: 17), textAlpha: 0.9, textColor: .black)
+    let passwordLabel = UILabel(textLabel: "Пароль", fontLabel: UIFont.systemFont(ofSize: 17),
+                                textAlpha: 0.9, textColor: .black)
+    let confirmPasswordLabel = UILabel(textLabel: "Потвердите Пароль", fontLabel: UIFont.systemFont(ofSize: 17),
+                                       textAlpha: 0.9, textColor: .black)
     let confirmPasswordTf = UITextField(fontTf: UIFont(name: "Gill Sans", size: 15))
     var warningLabels = [UILabel]()
     let inputButton = UIButton(type: .system)
@@ -28,29 +34,26 @@ class RegisterViewController: UIViewController {
                                        fontLabel: UIFont(name: "Gill Sans", size: 22),
                                        textAlpha: 0.9, textColor: .black)
     let alreadyRegisterbutton = UIButton()
-    var activeTextField : UITextField? = nil
-    
+    var activeTextField: UITextField? = nil
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         createWarningLabel()
         allWarningisHiden()
         addNotificationObservers()
         setupElements()
         setConstraints()
     }
-    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         setupScrollView()
     }
-    
 }
 
 extension RegisterViewController {
-    
-    
+    // MARK: - Scroll view
     private func setupScrollView() {
-        
         view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -59,71 +62,72 @@ extension RegisterViewController {
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         scrollView.contentSize =  CGSize(width: view.frame.size.width + 2, height: view.frame.size.height + 100)
     }
-    
+    // MARK: - Setup elements
     private func setupElements() {
-        
+        userNameTF.tag = 1
+        phoneTf.tag = 2
+        passwordTf.tag = 3
+        confirmPasswordTf.tag = 4
+        passwordTf.textContentType = .oneTimeCode
+        confirmPasswordTf.textContentType = .oneTimeCode
+        passwordTf.isSecureTextEntry = true
+        confirmPasswordTf.isSecureTextEntry = true
         alreadyRegisterbutton.setTitle("Войти", for: .normal)
         alreadyRegisterbutton.setTitleColor(.red, for: .normal)
         alreadyRegisterbutton.titleLabel?.font = UIFont(name: "Gill Sans", size: 22)
         alreadyRegisterbutton.alpha = 0.81
-        
         inputButton.setTitle("Регистрация", for: .normal)
         inputButton.addTarget(self, action: #selector(touchesButton), for: .touchUpInside)
         inputButton.setTitleColor(.systemRed, for: .normal)
         inputButton.alpha = 0.81
         inputButton.titleLabel?.font = UIFont(name: "Gill Sans", size: 22)
-        
+        inputButton.addTarget(self, action: #selector(unwindFunc), for: .touchUpInside)
     }
-    
     @objc private func touchesButton() {
-        for i in 0..<4 {
-            warningLabels[i].isHidden = false
+        for item in 0..<4 {
+            warningLabels[item].isHidden = false
         }
     }
-    
+    @objc private func unwindFunc() {
+        self.dismiss(animated: true, completion: nil)
+    }
     private func setConstraints() {
         view.backgroundColor = .white
-        
         scrollView.addSubview(mainLogo)
         mainLogo.translatesAutoresizingMaskIntoConstraints = false
         scrollView.addSubview(mainLabel)
         mainLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let userNameStackView = UIStackView(arrangedSubviews: [userNameLabel,userNameTF,warningLabels[0]])
+        let userNameStackView = UIStackView(arrangedSubviews: [userNameLabel, userNameTF, warningLabels[0]])
         userNameStackView.axis = .vertical
         userNameStackView.spacing = 19
         userNameStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let phoneStackView = UIStackView(arrangedSubviews: [phoneLabel,phoneTf,warningLabels[1]])
+        let phoneStackView = UIStackView(arrangedSubviews: [phoneLabel, phoneTf, warningLabels[1]])
         phoneStackView.axis = .vertical
         phoneStackView.spacing = 19
         phoneStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel,passwordTf,warningLabels[2]])
+        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTf, warningLabels[2]])
         passwordStackView.axis = .vertical
         passwordStackView.spacing = 19
         passwordStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let confirmPasswordStackView = UIStackView(arrangedSubviews: [confirmPasswordLabel,confirmPasswordTf,warningLabels[3]])
+        let confirmPasswordStackView = UIStackView(arrangedSubviews: [confirmPasswordLabel,
+                                                                      confirmPasswordTf,
+                                                                      warningLabels[3]])
         confirmPasswordStackView.axis = .vertical
         confirmPasswordStackView.spacing = 19
         confirmPasswordStackView.translatesAutoresizingMaskIntoConstraints = false
         confirmPasswordStackView.widthAnchor.constraint(equalToConstant: (view.frame.width - 40)).isActive = true
-        let contentView = UIStackView(arrangedSubviews: [userNameStackView,phoneStackView,passwordStackView,confirmPasswordStackView,inputButton])
+        let contentView = UIStackView(arrangedSubviews: [userNameStackView, phoneStackView,
+                                                         passwordStackView, confirmPasswordStackView, inputButton])
         contentView.axis = .vertical
         contentView.spacing = 55
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        
         scrollView.addSubview(contentView)
-        
-        let bottomStack = UIStackView(arrangedSubviews: [alreadyRegisterlabel,alreadyRegisterbutton])
+        let bottomStack = UIStackView(arrangedSubviews: [alreadyRegisterlabel, alreadyRegisterbutton])
         bottomStack.axis = .vertical
         bottomStack.spacing = 10
         bottomStack.alignment = .center
-        
         bottomStack.translatesAutoresizingMaskIntoConstraints = false
         scrollView.addSubview(bottomStack)
-        
         // Cosntraints
         NSLayoutConstraint.activate([
             mainLogo.heightAnchor.constraint(equalTo: scrollView.heightAnchor, multiplier: 0.07),
@@ -141,15 +145,12 @@ extension RegisterViewController {
             bottomStack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
         ])
     }
-    
     private func allWarningisHiden() {
-        for i in 0..<4 {
-            warningLabels[i].isHidden = true
+        for item in 0..<4 {
+            warningLabels[item].isHidden = true
         }
     }
-    
     private func createWarningLabel() {
-        
         for _ in 0..<4 {
             let label = UILabel()
             label.text = "Неправильный формат"
@@ -159,62 +160,86 @@ extension RegisterViewController {
             warningLabels.append(label)
         }
     }
-    
     private func addNotificationObservers() {
-        
-        
         userNameTF.delegate = self
         passwordTf.delegate = self
         confirmPasswordTf.delegate = self
         phoneTf.delegate = self
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector:#selector(keyBoardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyBoardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
-    
 }
-
-
 extension RegisterViewController: UITextFieldDelegate {
-    
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        self.view.endEditing(true)}
     @objc func keyBoardWillShow(notification: NSNotification) {
-        guard  let keySize =  (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
-        
+        guard let keySize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
+            as? NSValue)?.cgRectValue else {return}
         var shouldMoveViewUp = false
-        
         if let activeTF = activeTextField {
             let bottomOfTf = activeTF.convert(activeTF.bounds, to: self.view).maxY
             let topOfKeyboard = self.view.frame.height - keySize.height
-            
             if bottomOfTf > topOfKeyboard {
                 shouldMoveViewUp = true
             }
         }
-        
         if shouldMoveViewUp {
             self.view.frame.origin.y = 100 - keySize.height
         }
-        
     }
-    
-    @objc func keyBoardWillHide(notification : Notification) {
+    @objc func keyBoardWillHide(notification: Notification) {
         self.view.frame.origin.y = 0
     }
-    
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-         self.view.endEditing(true)
-         return false
-     }
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
     func textFieldDidBeginEditing(_ textField: UITextField) {
         self.activeTextField = textField
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField.tag == 1 {
+            if !ValidateService.shared.validate(searchString: userNameTF.text!, mode: 3) {
+                warningLabels[0].isHidden = false
+                warningLabels[0].text = "Неккоректный ввод"
+            } else {
+                warningLabels[0].isHidden = true
+            }
+        }
+        
+        if textField.tag == 2 {
+            if !ValidateService.shared.validate(searchString: phoneTf.text!, mode: 1) {
+                warningLabels[1].isHidden = false
+                warningLabels[1].text = "Неккоректный ввод"
+            } else {
+                warningLabels[1].isHidden = true
+            }
+        }
+        if textField.tag == 3 {
+            if !ValidateService.shared.validate(searchString: passwordTf.text!, mode: 2) {
+                warningLabels[2].isHidden = false
+                warningLabels[2].text = "Неккоректный ввод"
+            } else {
+                warningLabels[2].isHidden = true
+            }
+        }
+        if textField.tag == 4 {
+            if !ValidateService.shared.comparePassword(firstPassword: passwordTf.text!, secondPassword: confirmPasswordTf.text!) {
+                warningLabels[3].isHidden = false
+                warningLabels[3].text = "Пароли не совпадают"
+            } else {
+                 warningLabels[3].isHidden = true
+            }
+        }
+        
         self.activeTextField = nil
     }
 }
---
+++ musicListApp/ValidateService.swift
@@ -0,0 +1,9 @@
+//
+//  ValidateService.swift
+//  musicListApp
+//
+//  Created by zz on 11.02.2022.
+//  Copyright © 2022 Vadim Vitkovskiy. All rights reserved.
+//
+
+import Foundation
